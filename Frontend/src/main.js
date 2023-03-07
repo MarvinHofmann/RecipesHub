45f5990844d065773823b5820177d6881d09b235
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-03-07 18:14:21 +0100

    Init Frontend with vite, vue and Bootstrap
    Created Vite project installed bootstrap via npm

---
+++ Frontend/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
