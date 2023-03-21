d54fd9a2fc196ce2a4bb95be0c031d8cf1fea6b2
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-03-21 11:58:48 +0100

    Rutes to get specific Recipe and All recipes from DB

--- Backend/routes/recipes.js
+++ Backend/routes/recipes.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 const authorization = require("../middleware/verifyToken")
 const { Recipe } = require("../models/recipeSchema")
 
-router.get("/randomRecipes", authorization, (req, res) => {
-    res.status(200).send("Random")
-});
-
 router.post("/addRecipe", authorization, async (req, res) => {
     if (!req.body) return res.status(400).send({ message: "No information send", code: "E1" })
     // Create final recipe
@@ -25,4 +21,21 @@ router.post("/addRecipe", authorization, async (req, res) => {
     })
     res.status(201).send({ message: "Recipe Created" })
 })
+
+
+router.get("/randomRecipes", authorization, (req, res) => {
+    res.status(200).send("Random")
+});
+
+router.get("/recipe/:id", authorization, async (req, res) => {
+    const recipe = await Recipe.findOne({ "_id": req.params.id }).exec();
+    if (!recipe) return res.status(400).send({ message: "No Recipe with that id", code: "E1" });
+    res.status(200).send(recipe)
+});
+
+router.get("/allRecipes", authorization, async (req, res) => {
+    const recipes = await Recipe.find().exec();
+    if (!recipes) return res.status(400).send({ message: "No Recipes in the DB", code: "E1" });
+    res.status(200).send(recipes)
+});
 module.exports = router
