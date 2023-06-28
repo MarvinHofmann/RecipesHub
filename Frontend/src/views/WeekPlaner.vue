fd89f5348f5f5867f7e54eb73648b8d8ab1fdd5c
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-06-28 19:48:15 +0200

    Update WeekPlaner.vue

--- Frontend/src/views/WeekPlaner.vue
+++ Frontend/src/views/WeekPlaner.vue
@@ -3,22 +3,22 @@
   <div class="container mt-5">
     <h3 class="mb-0">
       Dein Persönlicher Wochenplan
-      <i class="bi bi-pencil-square float-end fs-5 mt-1" type="button"></i>
     </h3>
     <div class="d-flex justify-content-center mt-3">
       <div class="row w-100">
         <div v-for="day in days" class="col-1 px-0">
-          <div class="card calendarElement" @click="this.onAddRecipeToData(day.date)">
-            <p class="p-0 ps-1 m-0 text-muted">{{ getFormattedDate(day.date) }}</p>
+          <div class="card calendarElement">
+            <div class="row mx-1">
+              <p class="col-9 p-0 ps-1 m-0 text-muted">{{ getFormattedDate(day.date) }}</p>
+              <div class="col-3" type="button" @click="this.onHandleEntry(null, day.date, 'addRecipe')"><i class="bi bi-plus-circle"></i></div>
+            </div>
             <div class="card-body px-1">
-              <div
-                v-for="recipe in day.recipes"
-                :style="{ backgroundColor: recipe.color }"
-                class="text w-100"
-                type="button"
-                @click.self="this.$router.push('/rezept/' + recipe.id)"
-              >
-                {{ recipe.title }}
+              <div v-for="recipe in day.recipes" type="button" @click.self="this.$router.push('/rezept/' + recipe.id)">
+                <div class="row mx-1">
+                  <div :style="{ backgroundColor: recipe.color }" class="text-left col-9 float-start" @click="this.onHandleEntry(recipe, null, 'goToElement')">{{ recipe.title }}</div>
+                  <i :style="{ backgroundColor: recipe.color }" type="button" @click="this.onHandleEntry(recipe, null, 'deleteElement')"
+                    class="float-end col-3 text-right text-danger bi bi-trash-fill"></i>
+                </div>
               </div>
             </div>
           </div>
@@ -55,31 +55,15 @@ export default {
             {
               title: "Rezept 1",
               color: "#1e97f3",
-              id: "641a280868e7825d5922cf33",
+              id: "641ed8f3250b03f73e5e2ca9",
             },
           ],
         },
         {
           date: new Date(),
-          recipes: [
-            {
-              title: "Rezept 1",
-              color: "#127f3e",
-            },
-          ],
         },
         {
           date: new Date(),
-          recipes: [
-            {
-              title: "Rezept 1",
-              color: "#1e97f3",
-            },
-            {
-              title: "Rezept 1",
-              color: "#11f33f",
-            },
-          ],
         },
         {
           date: new Date(),
@@ -101,41 +85,45 @@ export default {
         },
         {
           date: new Date(),
-          recipes: [
-            {
-              title: "Rezept 1",
-              color: "#2e97f3",
-            },
-          ],
         },
         {
           date: new Date(),
-          recipes: [
-            {
-              title: "Rezept 1",
-              color: "#1e67f3",
-            },
-          ],
         },
       ],
     };
   },
   methods: {
-    onAddRecipeToData(date) {
-      if (this.$router.options.history.state.current != "/wochenPlan") return;
-      console.log("Add to ", date);
-      const saveModal = document.getElementById("addToDate");
-      const modal = new Modal(saveModal);
-      this.dateToAdd = date;
-      modal.toggle();
+    onHandleEntry(recipe, date, mode) {
+      console.log(mode)
+      switch (mode) {
+        case "addRecipe":
+          if (this.$router.options.history.state.current != "/wochenPlan") return;
+          console.log("Add to ", date);
+          const saveModal = document.getElementById("addToDate");
+          const modal = new Modal(saveModal);
+          this.dateToAdd = date;
+          modal.toggle();
+          break;
+
+        case "deleteElement":
+          console.log("DELETE ", recipe);
+          break;
+
+        case "goToElement":
+          console.log("GOTO RECIPE ", recipe);
+          this.$router.push("/rezept/" + recipe.id)
+          break;
+      }
     },
     getFormattedDate(date) {
-      return new Date(date).toLocaleDateString("de-DE", {weekday: "short", day: "numeric" , month: "numeric"});
+      return new Date(date).toLocaleDateString("de-DE", { weekday: "short", day: "numeric", month: "numeric" });
     },
   },
-  async mounted(){
+  async mounted() {
     let dates = await getDates()
-    console.log(dates);
+    dates.forEach(date => {
+      console.log(date);
+    });
   }
 };
 </script>
@@ -161,10 +149,17 @@ export default {
   }
 }
 
-.text {
+.text-left {
   margin-top: 5px;
-  text-align: center;
-  border-radius: 5%;
+  border-top-left-radius: 5%;
+  border-bottom-left-radius: 5%;
+  color: white;
+}
+
+.text-right {
+  margin-top: 5px;
+  border-top-right-radius: 15%;
+  border-bottom-right-radius: 15%;
   color: white;
 }
 
