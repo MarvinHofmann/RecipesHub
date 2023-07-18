1f84654dc74e544fe26891489215426a2dff32ef
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2023-07-18 10:32:02 +0200

    pup

--- Backend/pdfCreation/pdfCreation.js
+++ Backend/pdfCreation/pdfCreation.js
@@ -25,6 +25,7 @@ module.exports = async function createRecipePDF(recipe) {
     }
 
     const browser = await puppeteer.launch({
+        executablePath: "/usr/src/app/node_modules/puppeteer-core/.local-chromium/linux-1045629",
         args: ['--no-sandbox'],
         headless: true
     });
